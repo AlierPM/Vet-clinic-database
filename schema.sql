baf4febfcf8f60602f20518d00db27d1596883fd
Alier  Philip Maguet <alierditmaguet@gmail.com>
2022-11-02 12:43:52 -0700

    Modified schema data types

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-  id INT BIGSERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  date_of_birth DATE NOT NULL,
-  escape_attempts INT  NOT NULL,
-  neutered BOOLEAN NOT NULL,
+  id INT,
+  name VARCHAR(255),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
   weight_kg DECIMAL
 );
+
