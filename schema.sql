a36fa03a97ed565b0e6b11eff65e76871a420abf
Alier  Philip Maguet <alierditmaguet@gmail.com>
2022-11-02 11:34:37 -0700

    Modified schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-  id BIGSSERIAL PRIMARY KEY,
+  id INT BIGSERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   date_of_birth DATE NOT NULL,
-  escape_attempts INT NOT NULL,
+  escape_attempts INT  NOT NULL,
   neutered BOOLEAN NOT NULL,
   weight_kg DECIMAL
 );
